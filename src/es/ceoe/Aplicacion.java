dca4e3d70229ee0650c7a723c27bea48494e0189
Jacinto García <jacintogl82@gmail.com>
2021-05-18 14:42:06 +0200

    Commit inicial

---
+++ src/es/ceoe/Aplicacion.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package es.ceoe;
+
+/**
+ * Aplicacion de consola de comandos
+ * 
+ * @author jgarcia7
+ *
+ */
+public class Aplicacion {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
